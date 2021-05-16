97f77634993de21a5a67eb3d7bfff141951dfeb1
Stefan Dorresteijn <stefan.dorresteijn@eye.security>
2021-05-16 12:37:05 +0200

    Create hero

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+</script>
+
+<main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+  <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+    <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+      <h1>
+        <span class="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
+          <span class="block text-gray-900">Stefan Dorresteijn</span>
+          <span class="block text-gray-500 text-sm font-light tracking-wide">Entrepreneur, developer, cybersecurity enthousiast</span>
+        </span>
+      </h1>
+      <div class="mt-5">
+        <div class="mb-3">
+          <p class="text-gray-500 text-sm">Co-founder & CTO</p>
+          <p class="text-gray-900 text-lg">Refrag LLC</p>
+        </div>
+        <div class="mb-3">
+          <p class="text-gray-500 text-sm">Co-founder & CTO</p>
+          <p class="text-gray-900 text-lg">Dorresteijn Media</p>
+        </div>
+        <div class="mb-3">
+          <p class="text-gray-500 text-sm">Full stack developer</p>
+          <p class="text-gray-900 text-lg">EYE Security</p>
+        </div>
+      </div>
+      <button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+        Let's Talk!
+      </button>
+    </div>
+    <div class="invisible lg:visible mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+      <div class="flex-1" id="hasBlueBG">
+        <img class="flex-1" src="images/StefanRender.png" id="stefan" alt="Stefan's beautiful face">
+      </div>
+    </div>
+  </div>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+  #hasBlueBG {
+    background-image: url('/images/bgblue.svg');
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  </style>
