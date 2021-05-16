dd68618594496e2b2e93278b58508dfcf91bd9ec
Stefan Dorresteijn <stefan.dorresteijn@eye.security>
2021-05-16 13:24:13 +0200

    Add interest blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
 </script>
 
-<main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+<main class="mt-12 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-16">
+  <!-- HERO -->
   <div class="lg:grid lg:grid-cols-12 lg:gap-8">
     <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
       <h1>
@@ -16,24 +17,70 @@
           <p class="text-gray-900 text-lg">Refrag LLC</p>
         </div>
         <div class="mb-3">
-          <p class="text-gray-500 text-sm">Co-founder & CTO</p>
+          <p class="text-gray-500 text-sm">Founder & CEO</p>
           <p class="text-gray-900 text-lg">Dorresteijn Media</p>
         </div>
-        <div class="mb-3">
-          <p class="text-gray-500 text-sm">Full stack developer</p>
+        <div>
+          <p class="text-gray-500 text-sm">Developer & Security Analyst</p>
           <p class="text-gray-900 text-lg">EYE Security</p>
         </div>
       </div>
-      <button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+      <button type="button" class="mt-5 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         Let's Talk!
       </button>
     </div>
-    <div class="invisible lg:visible mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+    <div class="hidden lg:visible mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
       <div class="flex-1" id="hasBlueBG">
         <img class="flex-1" src="images/StefanRender.png" id="stefan" alt="Stefan's beautiful face">
       </div>
     </div>
   </div>
+  <!-- END HERO -->
+  <!-- ENTREPRENEURSHIP -->
+  <div class="lg:float-right lg:w-10/12 bg-blue-50 rounded-3xl mt-9 lg:grid lg:grid-cols-12 lg:gap-8">
+    <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left flex flex-wrap content-center pl-12">
+      <div>
+        <h3 class="text-xl font-semibold mb-3 tracking-wide">Entrepreneurship</h3>
+        <p class="mb-3 font-light text-gray-700">Startups are a passion of mine<br />so I’m never without a side project or business</p>
+        <button class="text-black">See Projects</button>
+      </div>
+    </div>
+    <div class="invisible lg:visible pt-12 pb-12 pr-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+      <img src="images/teamwork.svg" alt="Teamwork">
+    </div>
+  </div>
+  <div class="clear-both"></div>
+<!-- END ENTREPRENEURSHIP -->
+<!-- Software Development -->
+<div class="lg:w-10/12 bg-red-50 rounded-3xl mt-9 lg:grid lg:grid-cols-12 lg:gap-8">
+  <div class="invisible lg:visible pt-12 pb-12 pr-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+    <img src="images/coding.svg" alt="Coding">
+  </div>
+  <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left flex flex-wrap content-center pl-12">
+    <div>
+      <h3 class="text-xl font-semibold mb-3 tracking-wide block">Software Development</h3>
+      <p class="mb-3 font-light text-gray-700 block">There are few things in the world that bring me<br />as much joy as creating applications that people love.</p>
+      <button class="text-black block">See Projects</button>
+    </div>
+  </div>
+</div>
+<div class="clear-both"></div>
+<!-- END Software Development -->
+  <!-- ENTREPRENEURSHIP -->
+  <div class="lg:float-right lg:w-10/12 bg-blue-50 rounded-3xl mt-9 lg:grid lg:grid-cols-12 lg:gap-8">
+    <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left flex flex-wrap content-center pl-12">
+      <div>
+        <h3 class="text-xl font-semibold mb-3 tracking-wide">Cybersecurity</h3>
+        <p class="mb-3 font-light text-gray-700">I love figuring out how things work,<br />so why not try to break them just a little?</p>
+        <button class="text-black">See Projects</button>
+      </div>
+    </div>
+    <div class="invisible lg:visible pt-12 pb-12 pr-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+      <img src="images/hacking.svg" alt="Hacking">
+    </div>
+  </div>
+  <div class="clear-both"></div>
+<!-- END ENTREPRENEURSHIP -->
 </main>
 
 <style global lang="postcss">
