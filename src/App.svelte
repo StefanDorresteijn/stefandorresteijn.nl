5b42c7aa57f03797c66683a4355a23b8e8d2db13
Stefan Dorresteijn <stefan.dorresteijn@eye.security>
2021-05-16 16:26:52 +0200

    Finish desktop version

--- src/App.svelte
+++ src/App.svelte
@@ -25,9 +25,9 @@
           <p class="text-gray-900 text-lg">EYE Security</p>
         </div>
       </div>
-      <button type="button" class="mt-5 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+      <a href="https://calendly.com/stefand" target="_blank" type="button" class="mt-5 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         Let's Talk!
-      </button>
+      </a>
     </div>
     <div class="hidden md:inline-block text-center mt-12 relative sm:max-w-lg sm:mx-auto md:mt-0 md:max-w-none md:mx-0 md:col-span-6 md:flex md:items-center">
       <div id="hasBlueBG">
@@ -42,7 +42,7 @@
       <div class="p-16">
         <h3 class="text-xl font-semibold mb-3 tracking-wide">Entrepreneurship</h3>
         <p class="mb-3 font-light text-gray-700">Startups are a passion of mine<br />so I’m never without a side project or business</p>
-        <button class="text-black">See Projects</button>
+        <!-- <button class="text-black">See Projects</button> -->
       </div>
     </div>
     <div class="hidden md:inline-block justify-center relative sm:max-w-lg sm:mx-auto md:mt-0 md:max-w-none md:mx-0 md:col-span-6 md:flex md:items-center">
@@ -52,7 +52,7 @@
   <div class="clear-both"></div>
 <!-- END ENTREPRENEURSHIP -->
 <!-- Software Development -->
-<div class="md:w-10/12 bg-red-50 rounded-3xl mt-6 md:mt-24 md:grid md:grid-cols-12 md:gap-8">
+<div class="md:w-10/12 bg-red-50 rounded-3xl mt-6 md:mt-16 md:grid md:grid-cols-12 md:gap-8">
   <div class="hidden md:inline-block justify-center relative sm:max-w-lg sm:mx-auto md:mt-0 md:max-w-none md:mx-0 md:col-span-6 md:flex md:items-center">
     <img src="images/coding.svg" alt="Coding" class="h-44">
   </div>
@@ -60,19 +60,19 @@
     <div class="p-16">
       <h3 class="text-xl font-semibold mb-3 tracking-wide block">Software Development</h3>
       <p class="mb-3 font-light text-gray-700 block">There are few things in the world that bring me<br />as much joy as creating applications that people love.</p>
-      <button class="text-black block">See Projects</button>
+      <!-- <button class="text-black block">See Projects</button> -->
     </div>
   </div>
 </div>
 <div class="clear-both"></div>
 <!-- END Software Development -->
 <!-- CYBER -->
-<div class="md:float-right md:w-10/12 bg-blue-50 rounded-3xl mt-6 md:mt-24 md:grid md:grid-cols-12 md:gap-8">
+<div class="md:float-right md:w-10/12 bg-blue-50 rounded-3xl mt-6 md:mt-16 md:grid md:grid-cols-12 md:gap-8">
   <div class="justify-center md:max-w-2xl md:mx-auto md:col-span-6 flex flex-wrap content-center">
     <div class="p-16">
       <h3 class="text-xl font-semibold mb-3 tracking-wide">Cybersecurity</h3>
       <p class="mb-3 font-light text-gray-700">I love figuring out how things work,<br />so why not try to break them just a little?</p>
-      <button class="text-black">See Projects</button>
+      <!-- <button class="text-black">See Projects</button> -->
     </div>
   </div>
   <div class="hidden md:inline-block justify-center relative sm:max-w-lg sm:mx-auto md:mt-0 md:max-w-none md:mx-0 md:col-span-6 md:flex md:items-center">
@@ -116,7 +116,7 @@
 </div>
 <!-- END CONTENT CREATION -->
 <!-- Availability -->
-<div class="mt-16">
+<div class="mt-24">
   <div class="md:grid md:grid-cols-12 md:gap-8">
     <div class="hidden md:block col-span-7">
       <img src="images/booking.svg" alt="calendar" />
@@ -125,13 +125,25 @@
       <h3 class="text-2xl text-gray-900 font-extrabold sm:text-3xl xl:text-4xl mb-3">Availability</h3>
       <p class="mb-3 font-light text-gray-700">While I’m currently employed at a full time position, I love helping people.</p>
       <p class="mb-3 font-light text-gray-700">So if you need a hand, a pair of eyes or a fragment of my brain, let’s talk!</p>
-      <button type="button" class="mt-5 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+      <a href="https://calendly.com/stefand" target="_blank" type="button" class="mt-5 inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         Let's Talk!
-      </button>
+      </a>
     </div>
   </div>
 </div>
 <!-- END Availability -->
+<!-- Stay in touch -->
+<div class="mt-24 text-center mb-16">
+  <h3 class="text-xl text-gray-900 font-semibold sm:text-2xl xl:text-3xl mb-3">Stay in touch!</h3>
+  <span class="text-gray-600 text-lg block">Den Haag, Netherlands</span>
+  <span class="text-gray-900 text-2xl font-semibold block">info@stefandorresteijn.nl</span>
+</div>
+<div class="text-center mb-16">
+  <a href="https://twitter.com/stefanjd" target="_blank" class="bg-blue-50 inline-block rounded-full p-8">
+    <img src="images/twitter.png" class="w-12 h-12" alt="Twitter logo" />
+  </a>
+</div>
+<!-- End stay in touch -->
 </main>
 
 <style global lang="postcss">
